0042598304ad2acc766f180673da844b06b6df1b
John Watson <johnwatson484@hotmail.com>
2022-10-21 17:27:00 +0100

    Add active flag

--- app/config/verify.js
+++ app/config/verify.js
@@ -2,14 +2,16 @@ const Joi = require('joi')
 
 // Define config schema
 const schema = Joi.object({
+  totalRetries: Joi.boolean().default(10),
   pollingInterval: Joi.number().default(10000), // 10 seconds
-  totalRetries: Joi.boolean().default(10)
+  pollingActive: Joi.boolean().default(true)
 })
 
 // Build config
 const config = {
+  totalRetries: process.env.TOTAL_RETRIES,
   pollingInterval: process.env.POLLING_INTERVAL,
-  totalRetries: process.env.TOTAL_RETRIES
+  pollingActive: process.env.POLLING_ACTIVE
 }
 
 // Validate config
