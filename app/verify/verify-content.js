84e8e3282022a7c62aed9b0b2de85ce47360a712
John Watson <johnwatson484@hotmail.com>
2022-10-23 12:52:03 +0100

    Add tests

--- app/verify/verify-content.js
+++ app/verify/verify-content.js
@@ -1,17 +1,22 @@
 const crypto = require('crypto')
 
 const verifyContent = (content, hash) => {
-  const fileHash = crypto.createHash('sha256').update(content).digest('hex')
-  const isValid = fileHash === hash
+  try {
+    const fileHash = crypto.createHash('sha256').update(content).digest('hex')
+    const isValid = fileHash === hash
 
-  if (isValid) {
-    console.log('File content successfully verified')
-  } else {
-    console.log('File verification failed')
-    console.log(`File hash: ${fileHash}`)
-    console.log(`Validation hash: ${hash}`)
+    if (isValid) {
+      console.log('File content successfully verified')
+    } else {
+      console.log('File verification failed')
+      console.log(`File hash: ${fileHash}`)
+      console.log(`Validation hash: ${hash}`)
+    }
+    return isValid
+  } catch (err) {
+    console.log('Error verifying content:', err)
+    return false
   }
-  return isValid
 }
 
 module.exports = verifyContent
