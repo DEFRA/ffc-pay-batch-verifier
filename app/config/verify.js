1e745f8720837e9f75e00a6be762344b52549fae
John Watson <johnwatson484@hotmail.com>
2022-10-24 17:05:05 +0100

    Remove pointlessnessess

--- app/config/verify.js
+++ app/config/verify.js
@@ -1,25 +1,21 @@
 const Joi = require('joi')
 
-// Define config schema
 const schema = Joi.object({
   totalRetries: Joi.number().integer().default(15),
   pollingInterval: Joi.number().default(10000), // 10 seconds
   pollingActive: Joi.boolean().default(true)
 })
 
-// Build config
 const config = {
   totalRetries: process.env.TOTAL_RETRIES,
   pollingInterval: process.env.POLLING_INTERVAL,
   pollingActive: process.env.POLLING_ACTIVE
 }
 
-// Validate config
 const result = schema.validate(config, {
   abortEarly: false
 })
 
-// Throw if config is invalid
 if (result.error) {
   throw new Error(`The verification config is invalid. ${result.error.message}`)
 }
