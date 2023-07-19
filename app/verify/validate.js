f76e8d1ba3f37b4977bfc6452c3a3535fc9cf6c9
Simon Dunn <simondunn90@gmail.com>
2023-07-19 12:00:37 +0100

    validate glos file

--- app/verify/validate.js
+++ app/verify/validate.js
@@ -2,9 +2,15 @@ const verifyContent = require('./verify-content')
 const getFiles = require('./get-files')
 const success = require('./success')
 const failure = require('./failure')
+const { validateGlosFiles } = require('./validate-glos-files')
 
 const validate = async (pendingFilenames, processedFilenames) => {
-  const [checksumFile, batchFile] = await getFiles(pendingFilenames)
+  const [checksumFile, batchFile, controlFile] = await getFiles(pendingFilenames)
+
+  if (pendingFilenames.batchFilename.match(/_FCAP_/gm)) {
+    console.log('Identified FC file, validating against control file')
+    validateGlosFiles(batchFile, controlFile) ? console.log('FC file valid') : await failure(pendingFilenames)
+  }
 
   if (verifyContent(batchFile, checksumFile)) {
     await success(pendingFilenames, processedFilenames)
